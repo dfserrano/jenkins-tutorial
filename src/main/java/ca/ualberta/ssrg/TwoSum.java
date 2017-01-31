a8b9ad1f22cbf5ef340ee703d7c80829ca39d3b7
Diego Serrano <dfserrano@gmail.com>
2017-01-31 12:00:05 -0700

    Added JUnit dependency to pom

--- src/main/java/ca/ualberta/ssrg/TwoSum.java
+++ src/main/java/ca/ualberta/ssrg/TwoSum.java
@@ -1,9 +1,47 @@
 package ca.ualberta.ssrg;
 
+import java.util.Hashtable;
+
 public class TwoSum {
 
 	public static int[] getPositions(int[] nums, int target) {
+
+		Hashtable<Integer, Integer> mem = new Hashtable<Integer, Integer>();
+
+		for (int i = 0; i < nums.length; i++) {
+			mem.put(nums[i], i);
+
+			if (mem.containsKey(target - nums[i])) {
+				int[] result = { mem.get(target - nums[i]), i };
+				return result;
+			}
+		}
+
 		return null;
+	}
+
+	public static void main(String... args) {
+		if (args != null && args.length == 2) {
+
+			String[] pieces = args[0].split(",");
+			int[] nums = new int[pieces.length];
+
+			for (int i = 0; i < pieces.length; i++) {
+				nums[i] = Integer.parseInt(pieces[i]);
+			}
+
+			int target = Integer.parseInt(args[1]);
+
+			int[] result = getPositions(nums, target);
+
+			if (result != null) {
+				System.out.println("The positions are: " + result[0] + " and " + result[1]);
+			} else {
+				System.out.println("An error has occurred");
+			}
+		} else {
+			System.out.println("Usage: TwoSum <array of comma separated integers> <target>\nExample: TwoSum 1,2,3 5");
+		}
 	}
 
 }
--- src/test/java/ca/ualberta/ssrg/TwoSumTest.java
+++ src/test/java/ca/ualberta/ssrg/TwoSumTest.java
@@ -1,19 +1,18 @@
 package ca.ualberta.ssrg;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class TwoSumTest {
 
 	@Test
-	public void test() {
+	public void testSimpleGetPositions() {
 		int[] nums = {2, 7, 11, 15};
 		int target = 9;
 
 		int[] positions = TwoSum.getPositions(nums, target);
+		assertTrue(positions != null);
 		assertTrue(positions[0] == 0 && positions[1] == 1);
 	}
 
